071855f268456fb175beaa30f790f10c67492b65
Janak Mandavgade <76395030+janakmandavgade@users.noreply.github.com>
2026-03-17 20:08:36 +0530

    Update Scrollingtext to use my name instead.

--- src/components/scrollingText/Scrollingtext.jsx
+++ src/components/scrollingText/Scrollingtext.jsx
@@ -4,7 +4,7 @@ import Marquee from "react-fast-marquee";
 function ScrollingText(){
     return (
         <Marquee className="text-white bg-black mt-0.5">
-            Welcome to FreedomPlay
+            Welcome to Janak's online Lottery App!
         </Marquee>
     )
 }
